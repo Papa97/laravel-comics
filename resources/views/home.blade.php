94b9d81cde5f29f3725e108d1847e6107ff3e743
Guido <ppapaguido@gmail.com>
2022-02-02 17:36:48 +0100

    inserimento dinamico cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,16 @@
 @endsection
 
 @section('pageContent')
-    <h1>home page</h1>
-    @foreach ($comics as $comic)
-     <div class="card">
-         <img src="{{$comic['thumb']}}" alt="">
-         {{$comic['series']}}
-     </div>
-    @endforeach
+    <div class="series">Current Series</div>
+    <div class="container">
+        <div class="card-container">
+            @foreach ($comics as $comic)
+                <div class="card">
+                    <img src="{{$comic['thumb']}}" alt="">
+                    <p>{{$comic['series']}}</p>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,2 +1,7 @@
 <header>
+    <div class="container">
+        <ul>
+            <li>Characters</li>
+        </ul>
+    </div>
 </header>
